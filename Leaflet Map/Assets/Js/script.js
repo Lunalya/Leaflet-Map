99d7fadd7e8be128df3c2d146f1a4ca4e1743964
Wendy <74055268+Lunalya@users.noreply.github.com>
2021-01-08 12:29:06 +0100

    Add files via upload

--- Leaflet Map/Assets/Js/script.js
+++ Leaflet Map/Assets/Js/script.js
@@ -1,81 +1,116 @@
+var mymap = L.map('mapid').setView([43.18289245551174, 3.0033091048731557], 14);
 
 
-let url = 'https://api.openweathermap.org/data/2.5/weather?q=Narbonne&units=metric&lang=fr&appid=b8c7e4d942816deac3460f5324c1f365';
 
-fetch(url).then((response =>
-  response.json().then((data) => {
-    console.log(data);
-    document.querySelector('#city').innerHTML = data.name;
-    document.querySelector('#temp').innerHTML = "<i class='fas fa-thermometer-half'></i>" + data.main.temp + '°';
-    document.querySelector('#humidity').innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
-    document.querySelector('#wind').innerHTML = "<i class='fas fa-wind'></i>" + data.wind.speed + 'km/h';
-  })
-));
-
-
-var mymap = L.map('mapid').setView([43.18411,3.00502], 14);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibHVuYWx5YSIsImEiOiJja2prNjN5YjkxcTg2MnhvN2N3ZmVvODFxIn0.T_OmfuT-U-u5rtApms4Fdw'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
 
 
-var tattoo = L.icon({
-    iconUrl: 'Assets/Img/Icons/tattoo.png',
-    iconSize: [50, 50],
-    iconAnchor: [10, 40],
-    popupAnchor: [10, -25],
+
+
+
+var viaDomitia = L.icon({
+    iconUrl: 'ASSETS/IMAGES/MARQUEURS/via.png',
+
+    iconSize:     [70, 65], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-var marker = L.marker([43.18031343498011, 3.0086079755172705], {icon: tattoo}).addTo(mymap);
-marker.bindPopup("<img src=Assets/Img/wam.png>" + "Shop privé de tatouage WAM").openPopup();
+
+L.marker([43.18377726999478, 3.0041662787910663], {icon: viaDomitia}).addTo(mymap)
+    .bindPopup('<h1>Via Domitia</h1> <br> <img src="ASSETS/IMAGES/LIEUX/via.jpg"> <br> La voie Domitienne (Via Domitia) est une voie romaine construite à partir de 118 av. J.-C. pour relier l’Italie à la péninsule Ibérique en traversant la Gaule narbonnaise.')
+    .openPopup();
 
 
 
-var piercing = L.icon({
-    iconUrl: 'Assets/Img/Icons/piercing.png',
-    iconSize: [30, 30],
-    iconAnchor: [10, 40],
-    popupAnchor: [6, -35],
+
+
+
+
+var cultura = L.icon({
+    iconUrl: 'ASSETS/IMAGES/MARQUEURS/cultura.png',
+
+    iconSize:     [31, 50], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-var marker = L.marker([43.18250910938708, 3.0025977366914933], {icon: piercing}).addTo(mymap);
-marker.bindPopup("<img src=Assets/Img/simply.jpeg>" + "Simply Tattoo.").openPopup();
+
+    
+L.marker([43.17980518002467, 3.031314084323742], {icon: cultura}).addTo(mymap)
+    .bindPopup('<h1>Cultura</h1> <br> <img src="ASSETS/IMAGES/LIEUX/cultura.jpeg"> <br> Cultura est une enseigne de distribution française appartenant à la société Socultur, filiale du holding Sodival. Elle est la troisième enseigne spécialisée dans la commercialisation de biens et loisirs culturels et créatifs en France.')
+    .openPopup();    
 
 
 
-var beer = L.icon({
-    iconUrl: 'Assets/Img/Icons/beer.png',
-    iconSize: [50, 50],
-    iconAnchor: [10, 40],
-    popupAnchor: [13, -30],
+
+
+
+
+var mcDonald = L.icon({
+    iconUrl: 'ASSETS/IMAGES/MARQUEURS/macdo.png',
+
+    iconSize:     [50, 60], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-var marker = L.marker([43.18263121111855, 3.0030886422673473], {icon: beer}).addTo(mymap);
-marker.bindPopup("<img src=Assets/Img/antre.jpg>" + "L'échoppe de l'antre.").openPopup();
+
+L.marker([43.18254137860739, 3.028184434260234], {icon: mcDonald}).addTo(mymap)
+    .bindPopup('<h1>McDonald\'s</h1> <br> <img src="ASSETS/IMAGES/LIEUX/macdo.jpeg"> <br> McDonald\'s est une chaîne de restauration rapide américaine.')
+    .openPopup();    
 
 
 
-var film = L.icon({
-    iconUrl: 'Assets/Img/Icons/film.png',
-    iconSize: [50, 50],
-    iconAnchor: [10, 40],
-    popupAnchor: [13, -25],
+
+
+
+
+
+
+var simplyTattoo = L.icon({
+    iconUrl: 'ASSETS/IMAGES/MARQUEURS/simply.svg',
+
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-var marker = L.marker([43.16471527198497, 2.9872659910762387], {icon: film}).addTo(mymap);
-marker.bindPopup("<img src=Assets/Img/cgr.jpg>" + "Cinéma CGR.").openPopup();
+
+L.marker([43.18240742784494, 3.0025548113129776], {icon: simplyTattoo}).addTo(mymap)
+    .bindPopup('<h1>Simply Tattoo</h1> <br> <img src="ASSETS/IMAGES/LIEUX/simply.jpeg"> <br> Chez Simply Tattoo, vous pourrez trouver un large choix de piercing et rencontrer des tatoueurs et un perceur exceptionnel.')
+    .openPopup();    
 
 
 
 
-var sushi = L.icon({
-    iconUrl: 'Assets/Img/Icons/sushi.png',
-    iconSize: [50, 50],
-    iconAnchor: [10, 40],
-    popupAnchor: [10, -25],
+
+
+
+
+
+var yvesRocher = L.icon({
+    iconUrl: 'ASSETS/IMAGES/MARQUEURS/yvesrocher.png',
+
+    iconSize:     [38, 50], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-var marker = L.marker([43.18103884564566, 3.005408833563267], {icon: sushi}).addTo(mymap);
-marker.bindPopup("<img src=Assets/Img/yoli.jpg>" + "Restaurant YOLI de sushi à volonté.").openPopup();
+
+L.marker([43.160765321492924, 2.981185582477126], {icon: yvesRocher}).addTo(mymap)
+    .bindPopup('<h1>Yves Rocher</h1> <br> <img src="ASSETS/IMAGES/LIEUX/yvesrocher.jpg"> <br>  Yves Rocher France est une entreprise de cosmétique fondée par Yves Rocher à La Gacilly et dont le siège social est situé à Rennes..')
+    .openPopup();    
+
+
+
+
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Narbonne&lang=fr&units=metric&appid=12278076c2110f13b6adc2aca47dee23').then((response) =>
+	response.json().then((data) => {
+		console.log(data);
+		document.querySelector('#city').innerHTML = data.name;
+		document.querySelector('#temp').innerHTML = Math.trunc(data.main.temp) + '°';
+		document.querySelector('#description').innerHTML = data.weather[0].description;
+		document.querySelector('#humidity').innerHTML = data.main.humidity + '% d\'humidité';
+		document.querySelector('#wind').innerHTML = 'Vitesse du vent estimée à ' + data.wind.speed + 'km/h';		
+	})
+);
